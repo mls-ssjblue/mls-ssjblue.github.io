5b764bb39f40ad354673577222ed7160d816b5b3
Susheel <mlsusheel@gmail.com>
2021-08-12 23:31:44 +0800

    About me text content

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,10 +5,14 @@ export const About = () => {
     <div className="about">
       <div className="about-content">
           <div className="rick-pic">
-
           </div>
-          <div className="text-content">
-              text
+          <div className="about-text">
+            <h1>About me</h1>
+              I am a full stack engineer with experience working in different industries ranging from banking to startups.
+              I have worked on building web applications across both the front end and back end. I am constantly
+              looking to challenge myself with a growth mindset and build some cool stuff!
+              <div style={{display:"flex",alignItems: "center"}}><p className="quote">"Stay hungry. Stay foolish"</p>  - Steve Jobs</div>
+
           </div>
       </div>
     </div>
