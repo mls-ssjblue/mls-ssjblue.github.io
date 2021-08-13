4a71001c43cdf13ce621fc4d03e17c6ec0ad82bc
mlssjblue <mlsusheel@gmail.com>
2021-08-13 09:23:05 +0800

    Infinite rotation svg

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,20 +1,28 @@
 import "./about.css";
+import {ReactComponent as Svg} from "../../assets/circle-spin.svg";
 
+// Svg from '../../assets/ellipse.svg'
 export const About = () => {
-  return (
-    <div className="about">
-      <div className="about-content">
-          <div className="rick-pic">
-          </div>
-          <div className="about-text">
-            <h1>About me</h1>
-              I am a full stack engineer with experience working in different industries ranging from banking to startups.
-              I have worked on building web applications across both the front end and back end. I am constantly
-              looking to challenge myself with a growth mindset and build some cool stuff!
-              <div style={{display:"flex",alignItems: "center"}}><p className="quote">"Stay hungry. Stay foolish"</p>  - Steve Jobs</div>
+    return (
+        <div className="about">
+            <div className="about-content">
+                <div className="rick-pic">
+                </div>
+                <div className="about-text">
+                    <h1>About me</h1>
+                    I am a full stack engineer with experience working in different industries ranging from banking to
+                    startups.
+                    I have worked on building web applications across both the front end and back end. I am constantly
+                    looking to challenge myself with a growth mindset and build some cool stuff!
+                    <div style={{display: "flex", alignItems: "center"}}><p className="quote">"Stay hungry. Stay
+                        foolish"</p> - Steve Jobs</div>
 
-          </div>
-      </div>
-    </div>
-  );
+                </div>
+
+            </div>
+            <div className="about-background">
+                <Svg className="svg-1"/>
+            </div>
+        </div>
+    );
 };
