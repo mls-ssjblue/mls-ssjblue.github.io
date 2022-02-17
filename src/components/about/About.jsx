d9729b332f42d76327a03fc93b04c9f1380b5113
mlssjblue <mlsusheel@gmail.com>
2022-02-17 12:32:05 +0800

    Add favicon to github pages

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -9,7 +9,7 @@ export const About = () => {
             <div className="about-content">
                 <div className="about-text">
                     <h1>About me</h1>
-                    I am a full stack engineer with experience working in different industries ranging from banking to
+                    I am a full stack engineer and lead with experience working in different industries ranging from banking to
                     startups.
                     I have worked on building web applications across both the front end and back end. I am constantly
                     looking to challenge myself with a growth mindset and build some cool stuff!
